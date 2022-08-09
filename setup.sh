d25f25045e72e495a4e236b2316c7560c0791d6a
Igor Romanov <romigor96@gmail.com>
2022-08-09 16:30:30 +0300

    Working veth network setup

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-sudo ip link add virt0 type dummy
-sudo ip link add virt1 type dummy
-sudo ifconfig virt0 hw ether 62:cd:aa:7a:17:c6
-sudo ifconfig virt1 hw ether 62:cd:aa:7a:17:c7
-sudo brctl addbr br0
-sudo brctl addbr br0
-sudo brctl addif br0 virt0
-sudo brctl addif br0 virt1
-sudo ip link set br0 up
+sudo ip link add virt0 address 62:cd:aa:7a:17:c6 type veth peer name virt0s
+sudo ip link add virt1 address 62:cd:aa:7a:17:c7 type veth peer name virt1s
+sudo ip link add br0 type bridge
+sudo ip link set virt0s master br0
+sudo ip link set virt1s master br0
 sudo ip link set virt0 up
+sudo ip link set virt0s up
 sudo ip link set virt1 up
+sudo ip link set virt1s up
+sudo ip link set br0 up
 ip link
